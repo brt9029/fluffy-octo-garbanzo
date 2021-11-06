3d4c0f95bc6f0d598b8c11bf867e20d36e6475d9
brt9029 <brt9029@gmail.com>
2021-11-06 12:17:36 -0500

    scheduler fully functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,85 @@
 let dateToday = moment().format("MMM DD YYYY");
 $('#currentDay').append(dateToday);
 
-let currentTime = moment().format("H:m")
+let currentTime = moment().format("hA");
 
-let schedule = [];
+let oldSchedule = [{
+        time: "900",
+        task: ""
+    },
+    {
+        time: "1000",
+        task: ""
+    },
+    {
+        time: "1100",
+        task: ""
+    },
+    {
+        time: "1200",
+        task: ""
+    },
+    {
+        time: "1300",
+        task: ""
+    },
+    {
+        time: "1400",
+        task: ""
+    },
+    {
+        time: "1500",
+        task: ""
+    },
+    {
+        time: "1600",
+        task: ""
+    },
+    {
+        time: "1700",
+        task: ""
+    },
+];
 
-$('.btn').click(function() {
-    let text = $("#event").text().trim();
-    console.log(text);
-    
+$('.btn').on("click",function() {
+    let time = $(this).attr("id");
+    let task = $(this).siblings("input").val();
+
+    updateTask(time, task);
     saveTasks();
 });
+
+function updateTask(time, task) {
+    for(let i = 0; i < schedule.length; i++){
+        if(time === schedule[i].time){
+            schedule[i].task = task;
+        }
+    }
+};
 
 function saveTasks() {
     localStorage.setItem("schedule", JSON.stringify(schedule));
 };
 
 function loadTasks () {
-    JSON.parse(localStorage.getItem("schedule"));
+    schedule = JSON.parse(localStorage.getItem("schedule"));
 
-    $.each(schedule, function(index){
-        console.log(index);
-        console.log(schedule[index].time);
-        console.log(schedule[index].task);
-    });
-};
-
-function timeCheck() {
-    let time = moment().format("hA");
-    console.log(time);
-};
-
-setInterval(function(){
-    if(time.isAfter($(".hour").text().trim())){
-        $(".description").addClass("bg-danger");
+    for(let i = 0; i < schedule.length; i++){
+        if(schedule[i].task.length > 0){
+            $(`#${schedule[i].time}`).val(schedule[i].task);
+        }
     }
-}, 1800000);
+};
 
-// load previously saved tasks on the schedule on page load
+// setInterval(function(){
+//     if(moment(`${currentTime}`).isAfter()){
+//         $(`#${schedule.time}`).addClass("bg-danger");
+//     }
+// }, 10);
+
+function checkTask() {
+
+};
+
+// load previously saved tasks on the schedule on page load 1800000
 loadTasks();
